c63d0a8a055507a299e32155228381e93fe50d1f
beverm2391 <beverman4528@gmail.com>
2023-12-19 15:55:34 -0500

    updated python, added comments to CPP so far

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+template <int rows, int columns, int inners>
+inline void matmulImplNative(const float *left, const float *right, float *result)
+{
+    for (int row = 0; row < rows; row++) // for each row of the matrix A
+    {
+        for (int col = 0; col < columns; col++) // for each column of the matrix B
+        {
+            for (int inner = 0; inner < inners; inner++) // for each element of the row/column
+            {
+                result[row * columns + col] += left[row * columns + inner] * right[inner * columns + col];
+                // Result (ij) = A (ik) * B (kj) for each element of the row/column
+                // the matrix is flattened into A 1D array
+            }
+        }
+    }
 }
