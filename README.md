bef4857d53392b166bedee4247190a26b4d74145
beverm2391 <beverman4528@gmail.com>
2023-12-19 17:28:18 -0500

    update code, docs

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Fast Multidimensional Matrix Multiplication on CPU from Scratch
 
 This is my repo for following along to [Simon Boehm's](https://siboehm.com/about/) article, [Fast Multidimensional Matrix Multiplication on CPU from Scratch](https://siboehm.com/articles/22/Fast-MMM-on-CPU).
+
+### Compiler Optimizations
+- Using `-O3` for [optimization](https://clang.llvm.org/docs/CommandGuide/clang.html#cmdoption-O0)
+- Generating arch or march specific code with `-arch <architecture>` or `-march=<cpu>` for [architecture](https://clang.llvm.org/docs/CommandGuide/clang.html#cmdoption-march) and [microarchitecture](https://clang.llvm.org/docs/CommandGuide/clang.html#cmdoption-mcpu) respectively.
+- Using `-ffast-math` to allow the compiler to do associative floor math and promising that there will be no NaNs or Infs.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 
-
-
-template <int rows, int columns, int inners>
+// ! Native Implementation
+template <int rows, int columns, int inners> // template parameters, compile time constants
 inline void matmulImplNative(const float *left, const float *right, float *result)
+// left, right, and result point to the first element of matricies A, B, and C, respectively
+// the const keyword means that the function cannot modify the values of the pointers for mats A and B
 {
     for (int row = 0; row < rows; row++) // for each row of the matrix A
     {
@@ -16,4 +17,29 @@ inline void matmulImplNative(const float *left, const float *right, float *resul
             }
         }
     }
+}
+
+// ! Native Implementation with Register Accumulation
+// improved by performing the inner dot product in a register, rather than in memory
+template <int rows, int columns, int inners>
+inline void matmulImplNativeRegisterAcc(const float *left, const float *right, float *result)
+{
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            float acc = 0.0
+            for (int inner = 0; inner < inners; inner++)
+            {
+                acc += left[row * columns + inner] * right[inner * columns + col];
+            }
+            result[row * columns + col] = acc;
+        }
+    }
+}
+
+
+int main() {
+    // TODO: get some matricies and time each implementation
+    return 0; 
 }
